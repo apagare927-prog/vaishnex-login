5e0767835ccb43e563679b81954c470138657832
apagare927-prog <apagare927@gmail.com>
2026-01-30 21:54:55 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -15,4 +15,22 @@ preferredCountries:["in","us","gb"],
 utilsScript:
 "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.19/js/utils.js"
 
-});
+});/* DYNAMIC TITLES */
+
+const titles = [
+
+"Continue to Vaishnex",
+"Welcome to Vaishnex",
+"Start your journey",
+"Access your world",
+"Enter Vaishnex",
+"Hello again 👋",
+"Let’s get you in",
+"Your network awaits",
+"Step into Vaishnex",
+"Secure login"
+
+];
+
+document.getElementById("title").innerText =
+titles[Math.floor(Math.random()*titles.length)];
