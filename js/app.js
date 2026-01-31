6444f6d1b745b0d09e981b1c2defd7b2369abb1c
apagare927-prog <apagare927@gmail.com>
2026-01-31 08:36:53 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,70 +1,22 @@
-// COUNTRY AUTO DETECT
-const iti = window.intlTelInput(
-document.querySelector("#phone"),
-{
-initialCountry:"auto",
-geoIpLookup:function(callback){
-fetch("https://ipapi.co/json")
-.then(res=>res.json())
-.then(data=>callback(data.country_code))
-.catch(()=>callback("in"));
-}
-}
-);
+// CREATE ACCOUNT VALIDATION
 
-// TOGGLE
-const emailBtn=document.getElementById("emailBtn");
-const mobileBtn=document.getElementById("mobileBtn");
+const createForm = document.getElementById("createForm");
 
-const email=document.getElementById("email");
-const phone=document.getElementById("phone");
+if(createForm){
 
-phone.style.display="none";
+createForm.addEventListener("submit", function(e){
 
-emailBtn.onclick=()=>{
-email.style.display="block";
-phone.style.display="none";
-emailBtn.classList.add("active");
-mobileBtn.classList.remove("active");
-};
+const email = this.querySelector('input[type="email"]').value.trim();
+const mobile = this.querySelector('input[type="tel"]').value.trim();
 
-mobileBtn.onclick=()=>{
-email.style.display="none";
-phone.style.display="block";
-mobileBtn.classList.add("active");
-emailBtn.classList.remove("active");
-};
+if(email === "" && mobile === ""){
 
-// DYNAMIC HEADLINES
-const titles=[
-"Welcome to Vaishnex",
-"Secure access to Vaishnex",
-"Connect smarter with Vaishnex",
-"Your digital gateway",
-"Next-gen authentication"
-];
+e.preventDefault();
 
-document.getElementById("title")
-.innerText=titles[Math.floor(Math.random()*titles.length)];
+alert("Enter Email OR Mobile number");
 
-
-// EMPTY FIELD ERROR
-document.getElementById("loginBtn")
-.onclick=()=>{
-
-document.querySelectorAll(".input")
-.forEach(field=>{
-
-if(field.offsetParent!==null && !field.value){
-
-field.classList.add("error");
-
-setTimeout(()=>{
-field.classList.remove("error");
-},600);
-
+return false;
 }
 
 });
-
-};
+}
